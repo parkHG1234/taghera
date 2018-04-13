f5003844968d7f6499b579281dbe19575fe49e09
MC975-003 <mc975-003@MC975-003.local>
2018-04-13 16:59:22 +0900

    Initial Commit

---
+++ addplay_test1Tests/addplay_test1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  addplay_test1Tests.swift
+//  addplay_test1Tests
+//
+//  Created by MC975-003 on 2018. 4. 13..
+//  Copyright © 2018년 MC975-003. All rights reserved.
+//
+
+import XCTest
+@testable import addplay_test1
+
+class addplay_test1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
